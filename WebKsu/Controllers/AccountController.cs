0637317f3f1b8e078a14e1fc73772ce5262519d5
nashemistoart@gmail.com <nashemistoart@gmail.com>
2025-02-01 19:48:27 +0200

    controler account start

---
+++ WebKsu/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebKsu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
